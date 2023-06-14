1ed160869b335d93900389a0981799854e668946
gaabhenrique <74377524+gaabhenrique@users.noreply.github.com>
2023-06-14 14:56:06 -0300

    Atualização para usar o postgress do render

--- Server/src/app.module.ts
+++ Server/src/app.module.ts
@@ -3,18 +3,19 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ClientsModule } from './clients/clients.module';
+require('dotenv').config();
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'salao',
-      password: 'salao',
-      database: 'dbsalao',
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432'),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true, // Atenção: sincronização automática pode causar perda de dados em ambientes de produção
+      synchronize: true,
     }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
--- Server/src/main.ts
+++ Server/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
+require('dotenv').config();
 
 async function bootstrap() {
+  console.log(process.env);
   const app = await NestFactory.create(AppModule);
   app.use(cors());
   await app.listen(3000);
